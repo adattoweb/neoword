11d5dc52575ff58b0f47b678c99bb6b968dbe1de
adattoweb <adattostudio174@gmail.com>
2025-12-16 15:56:49 +0200

    added a lazy load for words

--- src/pages/Words/Words.jsx
+++ src/pages/Words/Words.jsx
@@ -1,15 +1,17 @@
 import "./Words.css"
 
-import { useEffect, useState } from "react"
+import { useEffect, useState, lazy, Suspense } from "react"
 import AddModal from "./AddModal"
-import Word from "./Word/Word"
 import Back from "../../components/Back/Back"
 import WordsHeader from "./WordsHeader"
 import { useBookStore } from "../../stores/useBookStore"
 import { useWordsStore } from "../../stores/useWordsStore"
 import { readLocal } from "../../helpers/readLocal"
+import { useLangStore } from "../../stores/useLangStore"
 
 export default function Words(){
+    const isEn = useLangStore(state => state.isEn)
+
     const bookID = useBookStore(state => state.bookID)
     const setBookID = useBookStore(state => state.setBookID)
 
@@ -38,6 +40,8 @@ export default function Words(){
     const onlyWords = {}
     sortedKeys.map(letter => Object.keys(words[letter]).map(key => onlyWords[key] = words[letter][key]))
 
+    const Word = lazy(() => import("./Word/Word"));
+
     return (
         <div className="words content">
             <Back onClick={() => setBookID(false)}/>
@@ -45,7 +49,9 @@ export default function Words(){
             <AddModal isOpen={isOpen} setIsOpen={setIsOpen}/>
             <div className="words__list slide">
                 <WordAdd/>
-                {sortedKeys.map(letter => Object.keys(words[letter]).map(key => <Word key={words[letter][key].word} ID={key} wordObj ={words[letter][key]} search={search} searchBy={searchBy} words={onlyWords} selected={selected}/>))}
+                <Suspense fallback={<div>{isEn ? "Loading words..." : "Завантаження слів"}</div>}>
+                    {sortedKeys.map(letter => Object.keys(words[letter]).map(key => <Word key={words[letter][key].word} ID={key} wordObj ={words[letter][key]} search={search} searchBy={searchBy} words={onlyWords} selected={selected}/>))}
+                </Suspense>
             </div>
         </div>
     )
