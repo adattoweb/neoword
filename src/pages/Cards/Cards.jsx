cf8e6f18ea5486ebaa6dca3f05dcddee5bf81371
adattoweb <adattostudio174@gmail.com>
2026-01-08 14:19:49 +0200

    fixed work progress bar in games, it reflects actual position

--- src/pages/Cards/Cards.jsx
+++ src/pages/Cards/Cards.jsx
@@ -36,7 +36,7 @@ export default function Cards(){
     const screenWidth = window.innerWidth
     const maxWidth = screenWidth > 230 && screenWidth > 700 ?  400 : screenWidth > 350 ? 300 : 230;
     // console.log(maxWidth, screenWidth)
-    let width = maxWidth * (id / wordsKeys.length)
+    let width = maxWidth * ((wordsKeys.length - rand.current + id) % wordsKeys.length / wordsKeys.length)
     function incrementId(isRight){
         console.log("====")
         if((id + 1) % wordsKeys.length === rand.current){
@@ -69,10 +69,6 @@ export default function Cards(){
         const inputRef = useRef()
         const translations = onlyWords[wordsKeys[id]].translations
         useEffect(() =>{
-            if(id === wordsKeys.length){
-                setIsOpen(true)
-                inputRef.current.disabled = true;
-            }
             if(translations.some(el => el.toLowerCase() === word.toLowerCase())){
                 setId(prev => (prev + 1) % wordsKeys.length)
                 rights.current++;
@@ -127,7 +123,7 @@ export default function Cards(){
             <div className="cards__content">
                 <div className="cardsheader">
                     <div className="cardsheader__info">
-                        <div className="cardsheader__total gradient">{isEn ? "Total" : "Всього"}: <span id="total">{id}/{wordsKeys.length}</span></div>
+                        <div className="cardsheader__total gradient">{isEn ? "Total" : "Всього"}: <span id="total">{(wordsKeys.length - rand.current + id) % wordsKeys.length}/{wordsKeys.length}</span></div>
                         <div className="cardsheader__reset gradient" onClick={reset}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
